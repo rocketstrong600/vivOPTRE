a069e8a120916edcc501d3e0a6d6c0a0c6c6516b
rocketstrong600 <rocketstrong600@gmail.com>
2024-01-18 01:43:07 +1000

    user action fix

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -14,15 +14,15 @@ class CfgVehicles
 
     		disableHeightLimit = 1;
     		maxLoadMass = 200000; // KG
-    		cargoAlignment[] = { "front", "center" };
+    		cargoAlignment[] = { "center", "front" };
 
     		cargoSpacing[] = { 0, 0.1, 0 };
 
-    		exits[] = { {0,-14,-3.5} };
+    		exits[] = { {0,-8,-3.5} };
 
     		unloadingInterval = 2;
-    		loadingDistance = 10;
-    		loadingAngle = 60;
+    		loadingDistance = 30;
+    		loadingAngle = 180;
     		parachuteClassDefault = "B_Parachute_02_F";
     		parachuteHeightLimitDefault	= 50;
 
@@ -31,10 +31,20 @@ class CfgVehicles
     class UserActions;
     class UserActions {
       class PelLift_LoadVehicle;
-      class PelLift_LoadVehicle {
-        displayName = "Load Vehicle Old";
-        displayNameDefault = "Load Vehicle Old";
-      };
+			class PelLift_LoadVehicle
+			{
+				userActionID=6;
+				displayName="Load Vehicle old";
+				displayNameDefault="Load Vehicle old";
+				textToolTip="Load Vehicle old";
+				position="cargo_door_handle";
+				showWindow=0;
+				radius=15;
+				priority=2;
+				onlyForPlayer=0;
+				condition="!(player in [gunner this, driver this]) AND (player == driver vehicle player) AND (str (this getVariable [""OPTRE_Pelican_AttachedToVehiclesEffect"",[]]) == ""[]"") AND (vehicle player != player)";
+				statement="0 = [this,vehicle player] spawn OPTRE_fnc_PelicanLoadValidate;";
+			};
     };
   };
 };
